322da666fb6ee213e2a7b926226ce7a840eeab1e
Diogo <up202006059@up.pt>
2022-01-06 12:12:21 +0000

    Added class Alien and modified Board to have Aliens, also made Tests for class Alien

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -8,49 +8,67 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
-
+  
   private int width, height;
   private Ship ship;
   private List<Wall> walls;
+  private List<Alien> aliens;
+  private boolean canAlienGoRight = true;
   private Information informations;
-
+  
   Board(int x, int y){
     width = x;
     height = y;
     ship = new Ship(49,50);
     this.walls = createWalls();
+    this.aliens = createAliens();
     this.informations = new Information(0,3,1);
   }
-
+  
   public int getWidth(){return width;}
   public int getHeight(){return height;}
-
+  public int getShipX(){return ship.getX();}
+  public boolean getMove(){return canAlienGoRight;}
+  public Alien getFirstAlien(){return aliens.get(0);}
+  
   public void processKey(KeyStroke key) {
     switch (key.getKeyType()) {
-      case ArrowLeft -> moveShip(ship.moveLeft());
-      case ArrowRight -> moveShip(ship.moveRight());
-      default -> moveShip(ship.Stand());
+      case ArrowLeft-> moveShip(ship.moveLeft());
+      case ArrowRight-> moveShip(ship.moveRight());
+      default-> moveShip(ship.Stand());
+    }
+    if(canAlienGoRight) {
+      for (Alien alien : aliens)
+        if (alien.close(width)) moveAlienDown();
+      moveAlienRight();
+    }
+    else {
+      for (Alien alien : aliens)
+        if (alien.close()) moveAlienDown();
+      moveAlienLeft();
     }
   }
-
+  
   public void draw(TextGraphics graphics){
     graphics.setBackgroundColor(TextColor.Factory.fromString("BLACK"));
     graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(width, height), ' ');
     for(Wall wall : walls)
       wall.draw(graphics);
+    for(Alien alien : aliens)
+      alien.draw(graphics);
     ship.draw(graphics);
     informations.draw(graphics);
   }
-
+  
   public void setPosition(Position position){
     ship.setPosition(position);
   }
-
+  
   public void moveShip(Position position) {
     if(canShipMove(position))
       ship.setPosition(position);
   }
-
+  
   public boolean canShipMove(Position position){
     if(position.getX()==width-1){
       ship.moveLeft();
@@ -62,11 +80,7 @@ public class Board {
     }
     return true;
   }
-
-  public int getShipX(){
-    return ship.getX();
-  }
-
+  
   private List<Wall> createWalls() {
     List<Wall> walls = new ArrayList<>();
     for(int i = 0; i < width; i++) {
@@ -74,5 +88,31 @@ public class Board {
       walls.add(new Wall(i,52));
     }
     return walls;
+  }
+  
+  private List<Alien> createAliens() {
+    aliens = new ArrayList<>();
+    for(int i = 0; i < 10; i += 2)
+      for(int j = 1; j < 40; j += 5){
+        aliens.add(new Alien(j,i+8));
+      }
+    return aliens;
+  }
+  
+  public void moveAlienDown() {
+    for(Alien alien : aliens) {
+      alien.setPosition(alien.moveDown());
+    }
+    canAlienGoRight = !canAlienGoRight;
+  }
+  
+  public void moveAlienLeft() {
+    for(Alien alien : aliens)
+      alien.setPosition(alien.moveLeft());
+  }
+  
+  public void moveAlienRight() {
+    for(Alien alien : aliens)
+      alien.setPosition(alien.moveRight());
   }
 }
--- src/test/java/TestBoard.java
+++ src/test/java/TestBoard.java
@@ -1,14 +1,8 @@
 import com.googlecode.lanterna.input.KeyStroke;
 import org.junit.jupiter.api.Test;
-import com.googlecode.lanterna.input.KeyType;
 
-
-import java.security.Key;
-import java.security.PKCS12Attribute;
-
-import static com.googlecode.lanterna.input.KeyType.ArrowLeft;
-import static com.googlecode.lanterna.input.KeyType.ArrowRight;
-import static org.junit.Assert.assertEquals;
+import static com.googlecode.lanterna.input.KeyType.*;
+import static org.junit.Assert.*;
 
 public class TestBoard {
     @Test
@@ -17,7 +11,7 @@ public class TestBoard {
         assertEquals(100, board.getWidth());
         assertEquals(50, board.getHeight());
     }
-
+    
     @Test
     public void testProcessKeyLeft(){
         KeyStroke key = new KeyStroke(ArrowLeft);
@@ -26,7 +20,7 @@ public class TestBoard {
         Position pos = new Position(48,50);
         assertEquals(pos.getX(), board.getShipX());
     }
-
+    
     @Test
     public void testProcessKeyRight(){
         KeyStroke key = new KeyStroke(ArrowRight);
@@ -35,35 +29,101 @@ public class TestBoard {
         Position pos = new Position(50,50);
         assertEquals(pos.getX(), board.getShipX());
     }
-
+    
+    @Test
+    public void testProcessKeyRandom(){
+        KeyStroke key = new KeyStroke(ArrowUp);
+        Board board = new Board(100,50);
+        board.processKey(key);
+        Position pos = new Position(49,50);
+        assertEquals(pos.getX(), board.getShipX());
+    }
+    
+    @Test
+    public void testSetPosition(){
+        Board board = new Board(100,50);
+        assertEquals(49,board.getShipX());
+        Position pos = new Position(1,1);
+        board.setPosition(pos);
+        assertEquals(1,board.getShipX());
+    }
+    
     @Test
     public void testMoveShip(){
         Board board = new Board(100,50);
-        Ship ship = new Ship(49, 40);
         Position pos = new Position(50, 40);
         board.moveShip(pos);
         assertEquals(pos.getX(), board.getShipX());
     }
-
+    
     @Test
     public void testCanShipMoveRight(){
         Board board = new Board(100,50);
         Position pos = new Position(99, 40);
-        assertEquals(false, board.canShipMove(pos));
+        assertFalse(board.canShipMove(pos));
     }
-
+    
     @Test
     public void testCanShipMoveLeft(){
         Board board = new Board(100,50);
         Position pos = new Position(1, 40);
-        assertEquals(false, board.canShipMove(pos));
+        assertFalse(board.canShipMove(pos));
     }
-
+    
     @Test
     public void testCanShipMoveTrue(){
         Board board = new Board(100,50);
         Position pos = new Position(49, 40);
-        assertEquals(true, board.canShipMove(pos));
+        assertTrue(board.canShipMove(pos));
     }
-
+    
+    @Test
+    public void testMoveAlienDown() {
+        Board board = new Board(100,50);
+        assertTrue(board.getMove());
+        board.moveAlienDown();
+        assertFalse(board.getMove());
+        board.moveAlienDown();
+        assertTrue(board.getMove());
+    }
+    
+    @Test
+    public void testMoveAlienRight() {
+        Board board = new Board(100,50);
+        Alien alien = board.getFirstAlien();
+        assertEquals(1, alien.getX());
+        board.moveAlienRight();
+        assertEquals(2, board.getFirstAlien().getX());
+    }
+    
+    @Test
+    public void testMoveAlienRightFar() {
+        Board board = new Board(100,50);
+        Alien alien = board.getFirstAlien();
+        assertEquals(1, alien.getX());
+        for(int i=0;i<10;i++)
+            board.moveAlienRight();
+        assertEquals(11, board.getFirstAlien().getX());
+    }
+    
+    @Test
+    public void testMoveAlienLeft() {
+        Board board = new Board(100,50);
+        Alien alien = board.getFirstAlien();
+        assertEquals(1, alien.getX());
+        board.moveAlienLeft();
+        assertEquals(0, board.getFirstAlien().getX());
+    }
+    
+    @Test
+    public void testMoveAlienLeftFar() {
+        Board board = new Board(100,50);
+        Alien alien = board.getFirstAlien();
+        assertEquals(1, alien.getX());
+        for(int i=0;i<20;i++)
+            board.moveAlienRight();
+        for(int i=0;i<10;i++)
+            board.moveAlienLeft();
+        assertEquals(11, board.getFirstAlien().getX());
+    }
 }
