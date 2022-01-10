6b95702c0e9951508670f917f61a2e0665db161e
Diogo <up202006059@up.pt>
2022-01-10 01:19:14 +0000

    added MenuWin, MenuWinView and Win class

--- src/main/java/gui/GameState.java
+++ src/main/java/gui/GameState.java
@@ -9,16 +9,19 @@ public class GameState {
   MainMenu start;
   MenuBoard board;
   MenuLoss loss;
+  MenuWin win;
   
   public GameState(GameView gui, int width, int height){
     start = new MainMenu(gui);
     board = new MenuBoard(gui, width, height);
     loss = new MenuLoss(gui);
+    win = new MenuWin(gui);
   }
   
   public void run() throws IOException {
     start.run();
     board.run();
     loss.run();
+    win.run();
   }
 }
