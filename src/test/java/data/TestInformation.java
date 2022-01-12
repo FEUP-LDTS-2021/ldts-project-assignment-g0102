fa568f5404ca5f7a65524c31acd6066dd57823e7
Diogo <up202006059@up.pt>
2022-01-12 21:48:05 +0000

    added tests

--- src/test/java/data/TestInformation.java
+++ src/test/java/data/TestInformation.java
@@ -1,64 +1,76 @@
 package data;
 
-import data.Information;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class TestInformation {
+    
+    private final Information info = new Information(0,3,1,1);
+    
     @Test
     public void testInformationConstructor(){
-        Information info = new Information(0,3, 1);
         assertEquals(0,info.getScore());
         assertEquals(3,info.getLives());
         assertEquals(1,info.getLevel());
+        assertEquals(1,info.getBullet());
     }
 
     @Test
     public void testGetLives() {
-        Information info = new Information(0,3,1);
         int lives = info.getLives();
         assertEquals(3,lives);
     }
 
     @Test
     public void testGetScore() {
-        Information info = new Information(0,3,1);
         int score = info.getScore();
         assertEquals(0,score);
     }
 
     @Test
     public void testGetLevel() {
-        Information info = new Information(0,3,1);
         int level = info.getLevel();
         assertEquals(1,level);
     }
-
+    
+    @Test
+    public void testGetBullet() {
+        int bullet = info.getBullet();
+        assertEquals(1,bullet);
+    }
+    
     @Test
     public void testLevelUp(){
-        Information info = new Information(0,3, 1);
-        int level_up = 2;
+        assertEquals(1, info.getLevel());
+        assertEquals(1, info.getBullet());
         info.levelUp();
-        int i = info.getLevel();
-        assertEquals(level_up, i);
+        assertEquals(2, info.getLevel());
+        assertEquals(2, info.getBullet());
     }
 
     @Test
     public void testScoreUp(){
-        Information info = new Information(0,3, 1);
-        int score_up = 100;
+        assertEquals(0,info.getScore());
         info.scoreUp();
-        int i = info.getScore();
-        assertEquals(score_up, i);
+        assertEquals(100,info.getScore());
     }
-
+    
     @Test
     public void testLiveHit(){
-        Information info = new Information(0,3, 1);
-        int live_hit = 2;
+        assertEquals(3,info.getLives());
         info.liveHit();
-        int i = info.getLives();
-        assertEquals(live_hit, i);
+        assertEquals(2,info.getLives());
+    }
+    
+    @Test
+    public void testOneUp(){
+        info.liveHit();
+        info.liveHit();
+        assertEquals(1,info.getLives());
+        info.OneUp();
+        assertEquals(2,info.getLives());
     }
 }
