0d20fe72cea8501d803e7593abe18cae7df1f603
CarlosMealha <up202005954@edu.fc.up.pt>
2022-01-16 13:56:12 -0800

    Font in terminal

--- src/main/java/gui/viewers/GameView.java
+++ src/main/java/gui/viewers/GameView.java
@@ -6,14 +6,18 @@ import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.swing.AWTTerminalFontConfiguration;
 
+import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 
 public class GameView {
   private final Screen screen;
   private final TextGraphics graphics;
   
-  public GameView(int width, int height) throws IOException {
+  public GameView(int width, int height) throws IOException, FontFormatException {
+    AWTTerminalFontConfiguration fontConfig = loadFontASB();
     TerminalSize terminalSize = new TerminalSize(width, height);
     DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
     Terminal terminal = terminalFactory.createTerminal();
@@ -24,7 +28,18 @@ public class GameView {
     screen.doResizeIfNecessary();     // Resize screen if necessary
     graphics = screen.newTextGraphics();
   }
-  
+
+  public AWTTerminalFontConfiguration loadFontASB() throws FontFormatException, IOException {
+    File fontFile = new File("src/main/resources/fonts/overkill.ttf");
+    Font font = Font.createFont(Font.TRUETYPE_FONT, fontFile);
+
+    GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+    ge.registerFont(font);
+
+    Font loadedFont = font.deriveFont(Font.PLAIN, 25);
+    return AWTTerminalFontConfiguration.newInstance(loadedFont);
+  }
+
   public Screen getScreen() {
     return screen;
   }
